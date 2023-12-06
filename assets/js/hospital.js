511096420da9f871de050f18ec7b600480c0dfa6
Aritan Huang <aritan840703@gmail.com>
2023-12-06 15:54:56 +0800

    新增[醫院loading.svg]

--- assets/js/hospital.js
+++ assets/js/hospital.js
@@ -141,15 +141,6 @@ function generatePagination(total, perPage) {
     const pageButtons = document.querySelectorAll('.page-link');
     //頁面上頁碼按鈕跑迴圈
     pageButtons.forEach(item => {
-        // 手機平板用戶點擊按鈕時會增加active
-        item.addEventListener('touchstart', function () {
-            this.classList.add('active');
-        });
-        // 手機平板用戶放開點擊按鈕時會移除active
-        item.addEventListener('touchend', function () {
-            // 移除活动状态类
-            this.classList.remove('active');
-        });
         item.addEventListener('click', function (e) {
             e.preventDefault();
             const page = e.target.getAttribute('data-id');
