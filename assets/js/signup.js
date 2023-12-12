41de7f348ba4725eb7089547cb42ffac2f33461a
Aritan Huang <aritan840703@gmail.com>
2023-12-12 23:50:27 +0800

    測試[imgur上傳圖片網域問題]

---
+++ assets/js/signup.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const clientId = '54125f1145322a9';
+const email = document.querySelector('#Email');
+const password = document.querySelector('#Password');
+const petName = document.querySelector('#petName');
+const petBreed = document.querySelector('#petBreed');
+const petBirthday = document.querySelector('#petBirthday');
+const address = document.querySelector('#address');
+const petPhoto = document.querySelector('#petPhoto');
+
+
+
+
+const signUpButton = document.querySelector('#signup-btn');
+
+petPhoto.addEventListener('change', function (e) {
+    const imgFile = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file-to-upload', imgFile);
+    axios.post('https://api.imgur.com/3/image', formData, {
+        headers: {
+            Authorization: `Client-ID ${clientId}`,
+            'Content-Type': 'multipart/form-data',
+        }
+    })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
