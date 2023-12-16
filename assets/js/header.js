d112299692fc103c52fd4ca2cc93b292dfc398e0
Aritan Huang <aritan840703@gmail.com>
2023-12-16 16:19:46 +0800

    新增[手機版登出]

--- assets/js/header.js
+++ assets/js/header.js
@@ -16,21 +16,37 @@ function checkLogIn() {
     const headerSignUp = document.querySelector('#header-signup');
     const headerLogIn = document.querySelector('#header-login');
     const headerLogOut = document.querySelector('#header-logout');
+    const mobileSignUp = document.querySelector('#mobile-signup');
+    const mobileLogIn = document.querySelector('#mobile-login');
+    const mobileLogOut = document.querySelector('#mobile-logout');
     if (token) {
         headerLogIn.classList.add('d-none');
         headerSignUp.classList.add('d-none');
         headerLogOut.classList.remove('d-none');
+        mobileSignUp.classList.add('disabled');
+        mobileLogIn.classList.add('d-none');
+        mobileLogOut.classList.remove('d-none');
     }
     else {
         headerLogIn.classList.remove('d-none');
         headerSignUp.classList.remove('d-none');
         headerLogOut.classList.add('d-none');
+        mobileSignUp.classList.remove('disabled');
+        mobileLogIn.classList.remove('d-none');
+        mobileLogOut.classList.add('d-none');
     }
 }
 checkLogIn();
 
 const btnLogOut = document.querySelector('#btn-logout');
+const btnMobileLogout = document.querySelector('#btn-mobile-logout');
+//電腦登出
 btnLogOut.addEventListener('click', e => {
+    e.preventDefault();
+    logOut();
+})
+//手機版登出
+btnMobileLogout.addEventListener('click', e => {
     e.preventDefault();
     logOut();
 })
