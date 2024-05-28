38ab2fc4c7f1ac48e079f85d691e5bf27b6d598a
Aritan Huang <aritan840703@gmail.com>
2024-05-28 16:23:43 +0800

    新增[健康紀錄post get功能]

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,9 @@
-export { apiUrl, imgurToken };
+export { apiUrl, imgurToken, userId, token };
 //測試
 // const apiUrl = `http://localhost:3000`;
 //正式
 const apiUrl = `https://pawmap-server.onrender.com`;
 const imgurToken = 'a0461da17c62e537d08e30497baa2ddd0d56a826';
+//取得登入後的userId 和 token
+const userId = localStorage.getItem('userId');
+const token = localStorage.getItem('token');
---
+++ assets/js/pet_health_record.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+import { apiUrl, userId, token } from "./config";
+
+let todoData = [];
+const todosList = document.querySelector('#todos-list');
+const newTodo = document.querySelector('#newTodo');
+const btnAddTodo = document.querySelector('#btn-add-todo');
+//初始化頁面
+function init() {
+    //取得使用者待辦清單
+    axios.get(`${apiUrl}/users/${userId}/todos`)
+        .then(res => {
+            todoData = res.data;
+            // console.log(todoData);
+            renderTodo();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+init();
+//渲染todo資料
+function renderTodo() {
+    let todoStr = '';
+    todoData.forEach(function (item) {
+        todoStr += `<li class="d-flex mb-2 justify-content-between">
+        <p class="d-flex align-items-center  me-4
+        me-md-5 fs-md-5"><span class="material-symbols-outlined me-1">
+                pet_supplies
+            </span>${item.created_at.substring(0, 10)}
+            ${item.task}</p>
+        <button type="button" class="btn btn-danger link-light" data-num="${item.id}">刪除</button>
+    </li>`
+    })
+    todosList.innerHTML = todoStr;
+}
+btnAddTodo.addEventListener('click', function (e) {
+    let todoObj = {};
+    todoObj.task = newTodo.value;
+    todoObj.created_at = new Date();
+    axios.post(`${apiUrl}/600/users/${userId}/todos`, todoObj, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+        .then(res => {
+            // console.log(res);
+            init();
+        })
+        .catch(err => {
+            Swal.fire("新增待辦失敗");
+        })
+    newTodo.value = '';
+})
