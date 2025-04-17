27d9b4e7cbecd7874cf55641eee3231af1435967
Aritan Huang <aritan840703@gmail.com>
2025-04-17 13:13:40 +0800

    切換[正式server]

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 export { apiUrl, imgurToken, userId, token, petName };
 //測試
-const apiUrl = `http://localhost:3000`;
+// const apiUrl = `http://localhost:3000`;
 //正式
-// const apiUrl = `https://pawmap-server.onrender.com`;
+const apiUrl = `https://pawmap-server.onrender.com`;
 const imgurToken = 'a0461da17c62e537d08e30497baa2ddd0d56a826';
 //取得登入後的userId 和 token
 const userId = localStorage.getItem('userId');
